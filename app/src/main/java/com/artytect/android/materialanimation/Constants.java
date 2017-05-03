5d0ffe63d59102a6f12475dc10257931aea2dbfd
artytim <arty.tim@gmail.com>
2017-05-03 11:16:14 +0800

    Material Animation - Ripple Animation  - Default Ripple Effect - Bounded &. Borderless Ripple  - Custom Ripple Effect - Bounded &. Borderless Ripple

---
+++ app/src/main/java/com/artytect/android/materialanimation/Constants.java
@@ -0,0 +1,11 @@
+package com.artytect.android.materialanimation;
+
+/**
+ * Created by timshum on 5/3/2017.
+ */
+
+public class Constants {
+    public static final String KEY_TYPE = "anim_type";
+    public static final String KEY_TITLE = "anim_title";
+    public static final String KEY_NAME = "anim_name";
+}
